d4c120edfe1b43112f2097ca2982f9413e5cce08
LOLGITHUBSz <56176903+LOLGITHUBSz@users.noreply.github.com>
2019-11-19 15:06:08 -0800

    LOL

--- MovieListAL.java
+++ MovieListAL.java
@@ -17,14 +17,28 @@ public class MovieListAL
      */
     public MovieListAL(Movie[] movies)
     {
+      Arrylist <Movie> r = new Arraylist<Movie>();
+      for(int i = 0 ; i<pool.size(); i++)
+      {
+        if( pool.get(i).getStudio().equals(studio)
 
-    }
-
+       
+      }
+     }
+     
     public ArrayList<Movie> getPool()
     {
-        return pool;
+        ArrayList<Movie> alm = new Arraylist<Movie>();
+        
+        for(int i = 0; i<pool.length() ; i++)
+        {
+          if( pool.get(i).getStudio().equals(studio))
+          {
+             Movie m = pool.get(i);
+             alm.add(m);
+        }
+      }
     }
-    
     /**
      * Given an arraylist of Movies and a studio name, create a new ArrayList of movies
      * by that studio
@@ -40,14 +54,10 @@ public class MovieListAL
      */
     public Movie getHighestrating(ArrayList<Movie> movies)
     {
-
-        
-        return null;
+      ArrayList<Movie> highestRated = new ArrayList<Movie>();
+      
         
     }
-    
-    
-
     
     /**
      * Find the highest movies by studio
@@ -56,10 +66,19 @@ public class MovieListAL
     {
         ArrayList<Movie> highestRated = new ArrayList<Movie>();
         
-        // Your code goes here
+        Movie mDisney = highestRated( getByStudio( "Disney"));
+        Movie mGhibli = highestRated( getByStudio( "Ghibli"));
+        Movie mIndy = highestRated( getByStudio( "Indy"));
+        ArrayList<Movie> answer = new ArrayList<Movie>();
+        answer.add(mDisney);
+        answer.add(mGhibli);
+        answer.add(mIndy);
+        return answer;
+              
+              
          return null;
         // return highestRated;
         
     }
-
+     
 }
